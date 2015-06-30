8555547e13f84af32118a0b41134a069d865ef52
Kristoffer Dalby <kradalby@kradalby.no>
2015-06-30 15:55:36 +0200

    auto recompile on code updates

--- emht.sh
+++ emht.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /opt/emht && git pull
-/opt/emht/target/universal/stage/bin/emht -DapplyEvolutions.default=true
+#/opt/emht/target/universal/stage/bin/emht -DapplyEvolutions.default=true
+cd /opt/emht && play run
